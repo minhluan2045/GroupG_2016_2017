3a20660a177c49c9b6a1e032fed19d3b241d2469
tranhoangkha131294 <tranhoangkha13121994@gmail.com>
2017-03-20 23:13:28 +0700

    Trần Hoàng Kha -module-báo cáo-chuyên đề web 1

---
+++ type-3/type-3.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <?php
+            if (!class_exists('lessc')) {
+                include ('./libs/lessc.inc.php');
+            }
+            $less = new lessc;
+            $less->compileFile('./less/type-3.less', 'css/type-3.css');
+        ?>
+        <link rel="stylesheet" type="text/css" href="css/type-3.css">
+        <link rel="stylesheet" type="text/js" href="js/bootstrap.min.js">
+
+    </head>
+
+    <body>
+        
+        <!--Search page-->
+        <div class="type-3">  
+            <div class="container">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <!--SEARCH-->
+                    <h3 class="section-big">TÌM KIẾM: </h3>              
+                    <div class="clear"></div>
+                    <!--/END SEARCH-->
+
+                    <div class="row">
+                        <!--PRODUCTS-->
+                        <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                            <div class="panel">
+                                <div class="panel-body">
+                                    <a href="#" class="zoom">
+                                        <img src="imagesproduct/kBgY_hoa-tuoi-bo-b106-jpg-20150819162815Ikeww5g1S.jpg" alt="" class="img-responsive">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="text-content">
+                                <h4><a href="#">GỬI TÌNH YÊU CỦA TÔI HB36</a></h4>
+                                <h4 class="pull-left" style="color: red;">500.000 VND</h4>
+                            </div>
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-success">
+                                    <a href="#" style="color: #fff">Mua</a>
+                                </button>
+                            </div>
+                        </div>
+                        <!--/END PRODUCTS-->
+
+                        <!--PRODUCTS-->
+                        <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                            <div class="panel">
+                                <div class="panel-body">
+                                    <a href="#" class="zoom">
+                                        <img src="imagesproduct/kBgY_hoa-tuoi-bo-b106-jpg-20150819162815Ikeww5g1S.jpg" alt="" class="img-responsive">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="text-content">
+                                <h4><a href="#">GỬI TÌNH YÊU CỦA TÔI HB36</a></h4>
+                                <h4 class="pull-left" style="color: red;">500.000 VND</h4>
+                            </div>
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-success">
+                                    <a href="#" style="color: #fff">Mua</a>
+                                </button>
+                            </div>
+                        </div>
+                        <!--/END PRODUCTS-->
+
+                        <!--PRODUCTS-->
+                        <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                            <div class="panel">
+                                <div class="panel-body">
+                                    <a href="#" class="zoom">
+                                        <img src="imagesproduct/0N6g_tinh-yeu-mi-bh153.jpg" alt="" width="100%" height="100%" class="img-responsive">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="text-content">
+                                <h4><a href="#">ANH YÊU EM HB35</a></h4>
+                                <h4 class="pull-left" style="color: red;">650.000 VND</h4>
+                            </div>
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-success">
+                                    <a href="#" style="color: #fff">Mua</a>
+                                </button>
+                            </div>
+                        </div>
+                        <!--/END PRODUCTS-->
+
+                        <!--PRODUCTS-->
+                        <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                            <div class="panel">
+                                <div class="panel-body">
+                                    <a href="#" class="zoom">
+                                        <img src="imagesproduct/rkeZ_Sac-Mau.jpg" alt="" width="100%" height="100%" class="img-responsive">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="text-content">
+                                <h4><a href="#">MÙA YÊU HB34</a></h4>
+                                <h4 class="pull-left" style="color: red;">440.000 VND</h4>
+                            </div>
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-success">
+                                    <a href="#" style="color: #fff">Mua</a>
+                                </button>
+                            </div>
+                        </div>
+                        <!--/END PRODUCTS-->
+
+                        <!--PRODUCTS-->
+                        <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                            <div class="panel">
+                                <div class="panel-body">
+                                    <a href="#" class="zoom">
+                                        <img src="imagesproduct/xuIz_hoa-tuoi-bo.jpg" alt="" width="100%" height="100%" class="img-responsive">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="text-content">
+                                <h4><a href="#">SẮC TÍM HB33</a></h4>
+                                <h4 class="pull-left" style="color: red;">350.000 VND</h4>
+                            </div>
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-success">
+                                    <a href="#" style="color: #fff">Mua</a>
+                                </button>
+                            </div>
+                        </div>
+                        <!--/END PRODUCTS-->
+    
+
+                        <!--PRODUCTS-->
+                        <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                            <div class="panel">
+                                <div class="panel-body">
+                                    <a href="#" class="zoom">
+                                        <img src="imagesproduct/W7Cs_bo_hoa_7.jpg" alt="" width="100%" height="100%" class="img-responsive">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="text-content">
+                                <h4><a href="#">TÌNH YÊU KHÔNG LỜI HB32</a></h4>
+                                <h4 class="pull-left" style="color: red;">380.000 VND</h4>
+                            </div>
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-success">
+                                    <a href="#" style="color: #fff">Mua</a>
+                                </button>
+                            </div>
+                        </div>
+                        <!--/END PRODUCTS-->
+
+                        <!--PRODUCTS-->
+                        <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                            <div class="panel">
+                                <div class="panel-body">
+                                    <a href="#" class="zoom">
+                                        <img src="imagesproduct/MaGq_tuyen-tap-hinh.jpg" alt="" width="100%" height="100%" class="img-responsive">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="text-content">
+                                <h4><a href="#">DUYÊN THẦM HB31</a></h4>
+                                <h4 class="pull-left" style="color: red;">400.000 VND</h4>
+                            </div>
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-success">
+                                    <a href="#" style="color: #fff">Mua</a>
+                                </button>
+                            </div>
+                        </div>
+                        <!--/END PRODUCTS-->
+
+                        <!--PRODUCTS-->
+                        <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                            <div class="panel">
+                                <div class="panel-body">
+                                    <a href="#" class="zoom">
+                                        <img src="imagesproduct/ckeR_Chớm-Nở-HB340.jpg" alt="" class="img-responsive">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="text-content">
+                                <h4><a href="#">ĐẮM SAY HH30</a></h4>
+                                <h4 class="pull-left" style="color: red;">600.000 VND</h4>
+                            </div>
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-success">
+                                    <a href="#" style="color: #fff">Mua</a>
+                                </button>
+                            </div>
+                        </div>
+                        <!--/END PRODUCTS--> 
+                    </div>
+
+                    <div class="row">
+                        <!--PAGINATION-->
+                        <div class="pagination pull-right">
+                            <ul>
+                                <li class="active">
+                                    <a href="#">1</a>
+                                </li>
+                                <li class="">
+                                    <a href="#">2</a>
+                                </li>
+                                <li class="">
+                                    <a href="#">3</a>
+                                </li>
+                                <li class="">
+                                    <a href="#">4</a>
+                                </li>
+                                <li><a href="#">Next</a></li>
+                            </ul>
+                        </div>
+                        <!--/END PAGINATION-->
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!--End Search page-->
+         
+    </body>
+</html>
